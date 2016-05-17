f857cca13a063640da347e6ea3f634cba4c0ce27
Daniël van der Ende <Danil van der Ende>
2016-05-17 13:57:42 +0200

    .gitignore & opzet meerdere seat statusen.
    De stoel statusen is nog niet totaal af omdat ik constant tegen een domme JS fout opliep. Hier is een link die wat handiger uitlegt hoe dit werkt.
    http://jsfiddle.net/jamietre/GjxCk/

--- WebProject78/WebProject78/js/mapControl.js
+++ WebProject78/WebProject78/js/mapControl.js
@@ -1,5 +1,28 @@
 ﻿var img = $('#room1');
 
+var selectedSeat = [null, null];
+
+var arraySeats = {};
+
+// rendering options for the 1st selection state
+var selected1opts = {
+        fillColor: '00ff00',
+        stroke: true,
+        strokeColor: '000000',
+        strokeWidth: 2
+};
+
+// rendering options for the 2nd selection state
+var selected2opts = {
+        fillColor: 'ff0000',
+        stroke: true,
+        strokeColor: '00ff00',
+        strokeWidth: 1
+};
+
+//Array for easy access to the rendering options
+var renderOpts = [selected1opts, selected2opts];
+
 var captions = {
     1: ['Seat number 1'],
     2: ['Seat number 2'],
@@ -19,14 +42,56 @@ var captions = {
 
 img.mapster({
     mapKey: 'seat',
-    singleSelect: true,
-    onMouseover: function (data) {
+    //mapValue: 'state',
+    isSelectable: false,
+
+    onGetList: getSeatList,
+
+    onClick: onSeatClick
+
+    /*onClick: function(data) {
         $('#seatText').text(captions[data['key']]);
     },
-    onMouseout: function (data) {
+    render_highlight: { fillColor: '000000', fade: false }*/
+    /*onMouseover: function (data) {
+        $('#seatText').text(captions[data['key']]);
+    },*/
+    /*onMouseout: function (data) {
         $('#seatText').text('');
-    }
+    }*/
 });
+
+function getSeatList(data) {
+    for (var i = 0; i < data.length; i++) {
+        //arraySeats[data.key] = data.value;
+        arraySeats[data.key] = 0;
+    }
+}
+
+function onSeatClick(data) {
+    /**
+     * 1. Set previous selected to it's main status
+     * 2. Store the currently selected seat and it's main status.
+     * 3. Update the appearance of the selected seat.
+     */
+
+    if (selectedSeat[1] != null) {
+        img.mapster('set', false, selectedSeat[0], renderOpts[selectedSeat[1]]);
+    }
+
+    selectedSeat[0] = data.key;
+    selectedSeat[1] = data.value;
+    
+    img.mapster('set', true, data.key, renderOpts[1]);
+    /*if (selectedSeat[1] != 0) {
+        img.mapster('set', true, selectedSeat[0], renderOpts[selectedSeat[1]]);
+    }*/
+
+    if (selectedSeat[1] != null) {
+        // now set the area state using the correct options
+        img.mapster('set', true, data.key, selectedSeat[1]);
+    }
+}
 
 function reserveSeat() {
     var selectedSeat = img.mapster('get');
@@ -42,4 +107,16 @@ function reserveSeat() {
     });
 
     document.getElementById('seatText').innerHTML = "Reserved seat: " + selectedSeat;
+}
+
+//Method for checking seats every 10 seconds.
+//setInterval(checkAllSeatStatus, 10000);
+
+function checkAllSeatStatus() {
+    //TODO: Get all visible seats and use checkSeatStatus on them.
+    alert("Checking seats!");
+}
+
+function checkSeatStatus(keySeat) {
+    //TODO: Add a method to check the seat in the database and update it if it changed.
 }
