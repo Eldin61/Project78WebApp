e0aa93a6a41ecdccc82ea03490839eab7ecd999e
JollyJavelin <Danvdende@gmail.com>
2016-05-20 01:46:46 +0200

    Status systeem afgemaakt en verduidelijkt

--- WebProject78/WebProject78/js/mapControl.js
+++ WebProject78/WebProject78/js/mapControl.js
@@ -1,7 +1,9 @@
 ﻿var img = $('#room1');
 
+//stores the key and state of the selected seat.
 var selectedSeat = [null, null];
 
+//stores the state of a seat to the seats key. Like a map.
 var arraySeats = {};
 
 // rendering options for the 1st selection state
@@ -20,8 +22,16 @@ var selected2opts = {
         strokeWidth: 1
 };
 
-//Array for easy access to the rendering options
-var renderOpts = [selected1opts, selected2opts];
+// rendering options for the selected selection state
+var selectedopts = {
+    stroke: true,
+    strokeColor: 'ff69b4',
+    strokeWidth: 1
+};
+
+//Array for easy access to the rendering options. More can be added.
+//0 is selected, 1 is free & 2 is reserved.
+var renderOpts = [selectedopts, selected1opts, selected2opts];
 
 var captions = {
     1: ['Seat number 1'],
@@ -40,71 +50,75 @@ var captions = {
     14: ['Seat number 14']
 }
 
+//
+/*for (var i = 1; i <= 14; i++) {
+    arraySeats[i] = Math.round(Math.random()) + 1;
+    console.log("Setup seat #" + i + " " + arraySeats[i]);
+    img.mapster("set", true, i, renderOpts[arraySeats[i]]);
+}*/
+
 img.mapster({
     mapKey: 'seat',
-    //mapValue: 'state',
+    mapValue: 'seatState',
     isSelectable: false,
 
-    onGetList: getSeatList,
-
-    onClick: onSeatClick
-
-    /*onClick: function(data) {
-        $('#seatText').text(captions[data['key']]);
+    //When the map is finished setting up this function is called to give all the seats a random state
+    //TODO: Automatically goes through all the seats and grabs the state from the DB.
+    //TODO: Update the seat image to the correct status.
+    onConfigured: function () {
+        for (var i = 1; i <= 14; i++) {
+            var id = Math.round(Math.random()) + 1;
+            arraySeats[i] = id;
+            console.log("Setup seaty #" + i + " " + arraySeats[i]);
+            //img.mapster("set", true, arraySeats[i], renderOpts[arraySeats[i]]);
+        }
     },
-    render_highlight: { fillColor: '000000', fade: false }*/
-    /*onMouseover: function (data) {
-        $('#seatText').text(captions[data['key']]);
-    },*/
-    /*onMouseout: function (data) {
-        $('#seatText').text('');
-    }*/
+    
+    onClick: onSeatClick
 });
 
-function getSeatList(data) {
-    for (var i = 0; i < data.length; i++) {
-        //arraySeats[data.key] = data.value;
-        arraySeats[data.key] = 0;
-    }
-}
-
 function onSeatClick(data) {
-    /**
-     * 1. Set previous selected to it's main status
-     * 2. Store the currently selected seat and it's main status.
-     * 3. Update the appearance of the selected seat.
-     */
-
-    if (selectedSeat[1] != null) {
-        img.mapster('set', false, selectedSeat[0], renderOpts[selectedSeat[1]]);
+    // 1. Set previous selected to it's main status
+    if (selectedSeat[0] != null) {
+        img.mapster("set", false, selectedSeat[0]);
+        if (selectedSeat[1] != null) {
+            img.mapster("set", true, selectedSeat[0], renderOpts[selectedSeat[1]]);
+        }
     }
 
+    // 2. Store the currently selected seat and it's main status.
     selectedSeat[0] = data.key;
-    selectedSeat[1] = data.value;
-    
-    img.mapster('set', true, data.key, renderOpts[1]);
-    /*if (selectedSeat[1] != 0) {
-        img.mapster('set', true, selectedSeat[0], renderOpts[selectedSeat[1]]);
-    }*/
+    selectedSeat[1] = arraySeats[selectedSeat[0]] || null;
 
-    if (selectedSeat[1] != null) {
-        // now set the area state using the correct options
-        img.mapster('set', true, data.key, selectedSeat[1]);
-    }
+    console.log("Selected seat: " + selectedSeat[0] + ", state: " + selectedSeat[1]);
+
+    // 3. Update the appearance of the selected seat.
+    img.mapster('set', false, selectedSeat[0], renderOpts[selectedSeat[1]]);
+    img.mapster('set', true, selectedSeat[0], renderOpts[0]);
 }
 
 function reserveSeat() {
-    var selectedSeat = img.mapster('get');
+    if (selectedSeat[0] == null) {
+        return false;
+    }
+    //var selectedSeat = img.mapster('get');
+    img.mapster("set", false, selectedSeat[0]);
 
     img.mapster('set_options', {
         areas: [
             {
-                key: selectedSeat,
-                staticState: true,
-                fillColor: 'ff000c'
+                key: selectedSeat[0],
+                options: renderOpts[2]
+                //staticState: true,
+                //fillColor: 'ff000c'
             }
         ]
     });
+
+    //TODO: Update change to DB.
+    arraySeats[selectedSeat[0]] = 2;
+
+    //TODO: Maybe Deselect the seat.
 
     document.getElementById('seatText').innerHTML = "Reserved seat: " + selectedSeat;
 }
