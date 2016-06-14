1e712f351771ea1ab7f0ce230405b30cd332b8b8
Daniël van der Ende <Danil van der Ende>
2016-06-14 23:10:00 +0200

    Gereserveerd data op map
    Werkt niet compleet.

--- WebProject78/WebProject78/js/mapControl.js
+++ WebProject78/WebProject78/js/mapControl.js
@@ -1,26 +1,29 @@
 ﻿var img = $('#room1');
 
-//stores the key and state of the selected seat.
-var selectedSeat = [null, null];
+function Seat(number, state) {
+    this.number = number;
+    //may only be "free", "reserved", "taken"
+    this.state = state;
+    this.userID = null;
+}
+
+Seat.prototype.getStateNr = function () {
+    console.log(this);
+    if (this.state === "free")
+        return 1;
+    else if (this.state === "reserved")
+        return 2;
+    else if (this.state === "taken")
+        return 3;
+    else 
+        return 0;
+}
+
+//stores the key, state and occupant of the selected seat.
+var selectedSeat = new Seat();
 
 //stores the state of a seat to the seats key. Like a map.
 var arraySeats = {};
-
-// rendering options for the 1st selection state
-var selected1opts = {
-        fillColor: '00ff00',
-        stroke: true,
-        strokeColor: '000000',
-        strokeWidth: 2
-};
-
-// rendering options for the 2nd selection state
-var selected2opts = {
-        fillColor: 'ff0000',
-        stroke: true,
-        strokeColor: '00ff00',
-        strokeWidth: 1
-};
 
 // rendering options for the selected selection state
 var selectedopts = {
@@ -29,9 +32,33 @@ var selectedopts = {
     strokeWidth: 1
 };
 
+// rendering options for the free state
+var freeopts = {
+    fillColor: '00ff00',
+    stroke: true,
+    strokeColor: '000000',
+    strokeWidth: 2
+};
+
+// rendering options for the reserved state
+var reservedopts = {
+    fillColor: 'ffa500',
+    stroke: true,
+    strokeColor: '00ff00',
+    strokeWidth: 1
+};
+
+// rendering options for the taken state
+var takenopts = {
+    fillColor: 'ff0000',
+    stroke: true,
+    strokeColor: 'ff0000',
+    strokeWidth: 1
+};
+
 //Array for easy access to the rendering options. More can be added.
 //0 is selected, 1 is free & 2 is reserved.
-var renderOpts = [selectedopts, selected1opts, selected2opts];
+var renderOpts = [selectedopts, freeopts, reservedopts, takenopts];
 
 var captions = {
     1: ['Seat number 1'],
@@ -62,17 +89,17 @@ img.mapster({
     //mapValue: 'seatState',
     isSelectable: false,
     highlight: false,
+    showToolTip: true,
     sortList: true,
 
     //When the map is finished setting up this function is called to give all the seats a random state
     //TODO: Automatically goes through all the seats and grabs the state from the DB.
     onConfigured: function () {
-        console.log(this);
         for (var i = 1; i <= 14; i++) {
             var id = Math.round(Math.random()) + 1;
-            arraySeats[i] = id;
-            console.log("Setup seat id:" + i + " status:" + arraySeats[i]);
-            img.mapster("set", true, String(i), renderOpts[arraySeats[i]]);
+            arraySeats[i] = new Seat(i, "taken");
+            console.log("Setup seat id:" + i + " status:" + arraySeats[i].getStateNr());
+            img.mapster("set", true, String(i), renderOpts[arraySeats[i].getStateNr()]);
         }
     },
 
@@ -87,51 +114,53 @@ img.mapster({
 });
 
 function onSeatClick(data) {
-    if (selectedSeat[0] !== data.key) {
+    if (selectedSeat.number !== data.key) {
         // 1. Set previous selected to it's main status
-        if (selectedSeat[0] != null) {
-            img.mapster("set", false, selectedSeat[0]);
-            if (selectedSeat[1] != null) {
-                img.mapster("set", true, selectedSeat[0], renderOpts[selectedSeat[1]]);
+        if (selectedSeat.number != null) {
+            img.mapster("set", false, selectedSeat.number);
+            if (selectedSeat.state != null) {
+                img.mapster("set", true, selectedSeat.number, renderOpts[selectedSeat.getStateNr()]);
             }
         }
 
-        // 2. Store the currently selected seat and it's main status.
-        selectedSeat[0] = data.key;
-        selectedSeat[1] = arraySeats[selectedSeat[0]] || null;
+        console.log("bla " + data.key);
 
-        console.log("Selected seat: " + selectedSeat[0] + ", state: " + selectedSeat[1]);
+        // 2. Store the currently selected seat and it's main status.
+        selectedSeat.number = data.key;
+        console.log($.grep(arraySeats, function (e, i) { return e.number == data.key; })[0]);
+        selectedSeat.state = $.grep(arraySeats, function (e, i) { return e.number == data.key; })[0]; //arraySeats.[selectedSeat.number] || null;
+
+        console.log("Selected seat: " + selectedSeat.number + ", state: " + selectedSeat.state);
 
         // 3. Update the appearance of the selected seat.
-        img.mapster('set', false, selectedSeat[0]);//, renderOpts[selectedSeat[1]]);
-        img.mapster('set', true, selectedSeat[0], renderOpts[0]);
+        img.mapster('set', false, selectedSeat.number);//, renderOpts[selectedSeat[1]]);
+        img.mapster('set', true, selectedSeat.number, renderOpts[0]);
     } else {
-        img.mapster("set", 'false', selectedSeat[0]);
-        img.mapster("set", true, selectedSeat[0], renderOpts[selectedSeat[1]]);
-        selectedSeat[0] = null;
-        selectedSeat[1] = null;
+        img.mapster("set", false, selectedSeat.number);
+        img.mapster("set", true, selectedSeat.number, renderOpts[selectedSeat.getStateNr]);
+        selectedSeat = new Seat();
     }
 }
 
 function reserveSeat() {
-    if (selectedSeat[0] == null) {
+    if (selectedSeat.number == null) {
         return false;
     }
     //var selectedSeat = img.mapster('get');
-    img.mapster("set", false, selectedSeat[0]);
+    img.mapster("set", false, selectedSeat.number);
 
     //TODO: Update change to DB.
-    arraySeats[selectedSeat[0]] = 2;
+    arraySeats[selectedSeat.number].state = "reserved";
 
     //updates changes to selectedSeat
-    selectedSeat[1] = 2;
+    selectedSeat.state = "reserved";
 
     console.log(selectedSeat);
 
 
-    img.mapster('set', true, selectedSeat[0], renderOpts[selectedSeat[1]]);
+    img.mapster('set', true, selectedSeat.number, renderOpts[selectedSeat.getStateNr()]);
 
-    document.getElementById('seatText').innerHTML = "Reserved seat: " + selectedSeat[0];
+    document.getElementById('seatText').innerHTML = "Reserved seat: " + selectedSeat.number;
 }
 
 //Method for checking seats every 10 seconds.
@@ -143,5 +172,6 @@ function checkAllSeatStatus() {
 }
 
 function checkSeatStatus(keySeat) {
+    
     //TODO: Add a method to check the seat in the database and update it if it changed.
 }
