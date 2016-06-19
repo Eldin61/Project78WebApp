f64d4ed872bcdc841df2ef51f6ccc57eb64dc42a
Orlando1991 <osd_1991@hotmail.com>
2016-06-19 14:52:04 +0200

    onlineusers fix

--- WebProject78/WebProject78/Controllers/HomeController.cs
+++ WebProject78/WebProject78/Controllers/HomeController.cs
@@ -15,18 +15,8 @@ namespace WebProject78.Controllers
         public ActionResult Index()
         {
 
-            ViewBag.Title="Project 78";
+            ViewBag.Title = "Project 78";
             employeeList();
-
-            //List<String> fakeUserList = new List<string>();
-            //fakeUserList.Add("Eldin");
-            //fakeUserList.Add("Daniel");
-            //fakeUserList.Add("Orlando");
-            //fakeUserList.Add("Mark");
-            //fakeUserList.Add("Rene");
-
-            //ViewBag.userList = fakeUserList;
-            //ViewBag.Title = "Project 78";
 
             return View();
         }
@@ -35,23 +25,51 @@ namespace WebProject78.Controllers
         {
             WebClient c = new WebClient();
             var data = c.DownloadString("http://145.24.222.188/p/index.php/werknemers");
-            System.Diagnostics.Debug.WriteLine(data);
 
             JObject o = JObject.Parse(data);
             JArray d = (JArray)o["data"]; //pakt alleen de data, verder nog index en item nodig
 
-            List<Tuple<JToken, String>> names = new List<Tuple<JToken, String>>();
-            for(var i = 0; i < d.Count; i++)
+            List<Tuple<String, String>> names = new List<Tuple<String, String>>();
+            for (var i = 0; i < d.Count; i++)
             {
 
-                names.Add(Tuple.Create(d[i]["UID"] ,d[i]["voornaam"] + " " + d[i]["achternaam"]));               
+                names.Add(Tuple.Create(d[i]["UID"].ToString(), d[i]["voornaam"] + " " + d[i]["achternaam"]));
             }
+            var confirmedusers = seatUsers();
+            var onlineusers = new List<String>();
+            var offlineusers = new List<String>();
+            var idlist = names.Select(t => t.Item1).ToList();
+
+            //offlineusers =
+            //    from nm in names
+            //    where !confirmedusers.Any(x => x.ToString() == nm.Item1)
+            //    select nm.Item2.ToList;
+
+
+            //ghetto for loop maar werkt, moet beter met linq
+            for (var i = names.Count - 1; i > -1; i--) //backwards loop laat stuff verwijderen maar behoudt OG index
+            {
+                for (var j = 0; j < confirmedusers.Count; j++)
+                {
+                    if (confirmedusers[j] == idlist[i])
+                    {
+                        onlineusers.Add(names[i].Item2);
+                        names.RemoveAt(i);
+                    }
+
+                }
+            }
+
+            offlineusers = names.Select(t => t.Item2).ToList();
+
+            onlineusers.Sort();
+            offlineusers.Sort();
+
             var tuplenames = names.Select(t => t.Item2).ToList(); //haalt namen uit de tuples met linq
 
-            seatUsers();
-            //ViewBag.nameList = tuplenames; 
-            System.Diagnostics.Debug.WriteLine("testcount: " + d.Count);
-            //System.Diagnostics.Debug.WriteLine("Achternaam: " + d[0]["achternaam"]);
+            ViewBag.onlineList = onlineusers;
+            ViewBag.offlineList = offlineusers;
+
         }
 
         public List<String> seatUsers()
@@ -64,24 +82,22 @@ namespace WebProject78.Controllers
             JObject o = JObject.Parse(data);
             JArray d = (JArray)o["data"];
 
-            for(var i = 0; i < d.Count; i++)
+            for (var i = 0; i < d.Count; i++)
             {
                 seats.Add(Tuple.Create(d[i]["B"].ToString(), d[i]["U"].ToString()));
             }
 
-            for(var i = 0; i < seats.Count; i++)
+            for (var i = 0; i < seats.Count; i++)
             {
-                if(seats[i].Item1 == "0")
+                if (seats[i].Item1 == "0")
                 {
                     seats.RemoveAt(i);
                 }
             }
             var userid = seats.Select(t => t.Item2).ToList();
-
-            ViewBag.nameList = userid;
             return userid;
         }
 
-  
+
     }
 }
